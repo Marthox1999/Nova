c19d5896854c45f9374f5c01a8ffcce974d0fceb
root <root@Avispa-Jaime>
2019-07-15 17:52:53 -0500

    Proveedores, sin mensajes, pero con back y front #58

--- apps/inventario/urls.py
+++ apps/inventario/urls.py
@@ -5,7 +5,9 @@ from inventario.views import *
 app_name='inventario'
 urlpatterns = [
     path('categoria', categoria, name='categoria'),
-    path('aniadirCategoria', aniadirCategoria, name='aniadirCategoria'),
+    path('categoriaCrear', aniadirCategoria, name='aniadirCategoria'),
     path('productos', productos, name='productos'),
-    path('aniadirProductos', aniadirProductos, name='aniadirProductos'),
+    path('productosCrear', aniadirProductos, name='aniadirProductos'),
+    path('proveedor', proveedor, name='proveedor'),
+    path('proveedorCrear', aniadirProveedor, name='aniadirProveedor'),
 ]
