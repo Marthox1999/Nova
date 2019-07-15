2297adf59fcf432146722d8d65c197620fdcc8a4
MMVLab <juan.felipe.gil@correounivalle.edu.co>
2019-07-15 14:54:07 -0500

    Agregado captura del input del nombre de la subCategoria para #53

--- apps/inventario/views.py
+++ apps/inventario/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def modificar_categoria(request, *args, **kwargs):
+    modificar = request.POST
+    print(modificar)
+    print(modificar.get('nombreSubcategoria'))
     return render(request, "inventario/modificar_categoria.html", {})
