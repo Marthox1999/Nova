0a6d0dbe574e4f21eb26f6372e711bb029d74e7d
valeriarm <valeria.rivera@correounivalle.edu.co>
2019-07-16 17:00:15 -0500

    se corrigio lo dicho en el pull request de los issues #2 y #50

--- apps/usuarios/views.py
+++ apps/usuarios/views.py
@@ -6,9 +6,7 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from usuarios.models import AdministradorDuenio
 # Create your views here.
-from usuarios.models import 
 from django.core.exceptions import ValidationError
-
 
 
 def ingreso(request):
