802de632f493b227867bb56fdc475781cb762e69
MMVLab <juan.felipe.gil@correounivalle.edu.co>
2019-07-15 20:00:54 -0500

    #53 agregado control para combobox de categorias

--- apps/inventario/views.py
+++ apps/inventario/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from inventario.models import Categoria, SubCategoria
 # Create your views here.
 
 def modificar_categoria(request, *args, **kwargs):
-    modificar = request.POST
-    print(modificar)
-    print(modificar.get('nombreSubcategoria'))
-    return render(request, "inventario/modificar_categoria.html", {})
+    categorias = Categoria.objects.all()
+    modificar = request.POST  
+    idCategoria = modificar.get('categoria')
+    subCategorias = {}
+    if(idCategoria=='-1'):
+        print("entro")
+        subCategorias = {}
+    else:
+        subCategorias = SubCategoria.objects.filter(fkCategoria=idCategoria)
+
+    context={'categorias':categorias, 'subCategorias':subCategorias}
+    return render(request, "inventario/modificar_categoria.html", context, {})
+
+def prueba(request, *args, **kwargs):
+    categorias = Categoria.objects.all()
+    context={'categorias':categorias}
+    print(request.POST)
+    return render(request, "prueba.html", context, {})
+    
