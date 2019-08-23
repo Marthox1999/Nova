4cd82ceae007122eea489598520ea95c1bbafde2
valeriarm <valeria.rivera@correounivalle.edu.co>
2019-08-22 19:00:04 -0500

    se corrigio un error

--- apps/inventario/urls.py
+++ apps/inventario/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('productosCategorias/<str:nombre>/<str:categoria>/', productosCategoriasVista, name='productosCategorias'),
     path('productosCategorias/<str:nombre>/<str:categoria>/<str:subCategoria>/', productosSubCategoriasVista, name='productosSubCategorias'),
     path('bodegaModificar', modificarBodega , name='bodegaModificar'),
+]
 
