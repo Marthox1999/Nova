c2e106c1d433c1c20e8151d4a11026ee3c1a27fa
Marthox1999 <mateo.gregory@correounivalle.edu.co>
2019-09-03 13:28:14 -0500

    Historia de usuario #A8 terminada, sujeta a pruebas

--- apps/reportes/views.py
+++ apps/reportes/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 
 from ventas.models import *
 from datetime import timedelta, date, datetime
+from dateutil import relativedelta
 
 def inicioReportes(request, *args, **kwargs):
     context = {}
@@ -14,9 +15,19 @@ def inicioReportes(request, *args, **kwargs):
             return redirect(to='reportes:reporteVentas')
     return render (request, "reportes/reportes.html", context, {})
 
-def daterange(start_date, end_date):
+def daterangeday(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
+
+def daterangemonth(start_date, end_date):
+    r = relativedelta.relativedelta(end_date, start_date).months
+    for n in range(r):
+        yield start_date + relativedelta.relativedelta(months=+n)
+
+def daterangeyear(start_date, end_date):
+    r = relativedelta.relativedelta(end_date, start_date).years
+    for n in range(r):
+        yield start_date + relativedelta.relativedelta(years=+n)
 
 def reporteVentas(request, *args, **kwargs):
     context = {}
@@ -30,13 +41,41 @@ def reporteVentas(request, *args, **kwargs):
         except:
             messages.info(request, 'Por favor ingrese una fecha valida')
             return render(request, "reportes/reporteVentas.html", context, {})
+        tipoReporte = int(data.get('tipoReporte'))
         dias = []
         cantidad = []
-        for dia in daterange(fechaInicio, fechaFin):
-            cant = Factura.objects.filter(fecha=dia).count()
-            cantidad.append(cant)
-            dias.append(dia.strftime('%Y-%m-%d'))
-        context={"datax":dias,"datay":cantidad, "fechaInicio":fechaInicio.strftime('%Y-%m-%d'), "fechaFin":fechaFin.strftime('%Y-%m-%d')}
-        return render(request, "reportes/reporteVentas.html", context, {})
-    
+        if (tipoReporte==1):
+            cant = 0
+            for dia in daterangeday(fechaInicio, fechaFin):
+                cant = Factura.objects.filter(fecha=dia).count()
+                cantidad.append(cant)
+                dias.append(dia.strftime('%Y-%m-%d'))
+            context={"datax":dias,"datay":cantidad, "fechaInicio":fechaInicio.strftime('%Y-%m-%d'), "fechaFin":fechaFin.strftime('%Y-%m-%d')}
+            return render(request, "reportes/reporteVentas.html", context, {})
+        if (tipoReporte==2):
+            fechaInicio = date(fechaInicio.year, fechaInicio.month, 1)
+            fechaFin = date(fechaFin.year, fechaFin.month, 1)
+            print(fechaInicio, ':' , fechaFin)
+            for mes in daterangemonth(fechaInicio, fechaFin):
+                cant = 0
+                print('month: ',mes)
+                for dia in daterangeday(mes, (mes + relativedelta.relativedelta(months=+1)) ):
+                    cant += Factura.objects.filter(fecha=dia).count()
+                cantidad.append(cant)
+                dias.append(mes.strftime('%Y-%m'))
+            context={"datax":dias,"datay":cantidad, "fechaInicio":fechaInicio.strftime('%Y-%m'), "fechaFin":fechaFin.strftime('%Y-%m')}
+            return render(request, "reportes/reporteVentas.html", context, {})
+        if (tipoReporte==3):
+            fechaInicio = date(fechaInicio.year, 1, 1)
+            fechaFin = date(fechaFin.year, 1, 1) + relativedelta.relativedelta(years=+1)
+            for year in daterangeyear(fechaInicio, fechaFin):
+                cant = 0
+                for dia in daterangeday(year, (year + relativedelta.relativedelta(years=+1)) ):
+                    cant += Factura.objects.filter(fecha=dia).count()
+                cantidad.append(cant)
+                print(year)
+                dias.append(year.strftime('%Y'))
+            context={"datax":dias,"datay":cantidad, "fechaInicio":fechaInicio.strftime('%Y'), "fechaFin":fechaFin.strftime('%Y')}
+            return render(request, "reportes/reporteVentas.html", context, {})
+
     return render (request, "reportes/reporteVentas.html", context, {})
--- apps/ventas/views.py
+++ apps/ventas/views.py
@@ -17,6 +17,14 @@ from datetime import datetime
 def descuentos(request, *args, **kwargs):
     return render(request, "ventas/descuentos.html", {})
 
+'''
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------CREAR DESCUENTOS--------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+'''
+
 def crearDescuento(request):
     categorias = Categoria.objects.all()
     context={
@@ -118,6 +126,161 @@ def crearDescuentoSubCategoria(request, idCategoria, idSubCategoria):
 
 
 def crearDescuentoProducto(request, idCategoria, idSubCategoria, idProducto):
+    categorias = Categoria.objects.all()
+    subcategorias = SubCategoria.objects.filter(fkCategoria=idCategoria)
+    productos = Producto.objects.filter(fkSubCategoria=idSubCategoria)
+    categoria = idCategoria
+    subcategoria = idSubCategoria
+    producto = idProducto
+    context={
+        'categorias':categorias, 
+        'subcategorias':subcategorias, 
+        'productos':productos,
+        'categoria':categoria,
+        'subcategoria':subcategoria,
+        'producto':producto
+        }
+
+    if request.method == 'POST':
+        data = request.POST
+        fechaInicio = data.get('fecha_inicio')
+        fechaFin = data.get('fecha_fin')
+        descuento = data.get('descuento')
+        producto = Producto.objects.get(pkProducto = idProducto)
+        newDescuento = DescuentoProducto(
+            fkProducto = producto,
+            fechaInicio = fechaInicio,
+            fechaFin = fechaFin,
+            porcentajeDescuento = descuento
+        )
+        auxfechafin = datetime.strptime(fechaFin,'%Y-%m-%d')
+        auxfechainicio = datetime.strptime(fechaFin,'%Y-%m-%d')
+        difFechas = (auxfechafin-auxfechainicio).days
+        fechaActual = (auxfechainicio-datetime.today()).days
+        if(difFechas < 0 or fechaActual < -1):
+            messages.info(request, 'La fecha de inicio no puede ser mayor que la fecha final')
+            return render(request, "ventas/creardescuentos.html", context,{})
+        try:
+            newDescuento.full_clean()
+        except:
+            messages.info(request, 'Alguno(s) campo(s) no son validos')
+            return render(request, "ventas/creardescuentos.html", context,{})
+        newDescuento.save()
+        messages.success(request, 'EL descuento ha sido creado exitosamente')
+        return redirect(to='ventas:descuentoCrear')
+
+    return render(request, "ventas/creardescuentos.html", context, {})
+
+'''
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------MODIFICAR DESCUENTOS----------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+'''
+
+
+def modificarDescuento(request):
+    categorias = Categoria.objects.all()
+    context={
+        'categorias':categorias, 
+        'subcategorias':[], 
+        'productos':[],
+        'categoria':0,
+        'subcategoria':0,
+        'producto':0
+        }
+    return render(request, "ventas/creardescuentos.html", context, {})
+
+def modificarDescuentoCategoria(request, idCategoria):
+    categorias = Categoria.objects.all()
+    subcategorias = SubCategoria.objects.filter(fkCategoria=idCategoria)
+    categoria = idCategoria
+    context={
+        'categorias':categorias, 
+        'subcategorias':subcategorias, 
+        'productos':[],
+        'categoria':categoria,
+        'subcategoria':0,
+        'producto':0
+        }
+
+    if request.method == 'POST':
+        data = request.POST
+        fechaInicio = data.get('fecha_inicio')
+        fechaFin = data.get('fecha_fin')
+        descuento = data.get('descuento')
+        categoria = Categoria.objects.get(pkCategoria = idCategoria)
+        newDescuento = DescuentoCategoria(
+            fkCategoria = categoria,
+            fechaInicio = fechaInicio,
+            fechaFin = fechaFin,
+            porcentajeDescuento = descuento
+        )
+        auxfechafin = datetime.strptime(fechaFin,'%Y-%m-%d')
+        auxfechainicio = datetime.strptime(fechaFin,'%Y-%m-%d')
+        difFechas = (auxfechafin-auxfechainicio).days
+        fechaActual = (auxfechainicio-datetime.today()).days
+        if(difFechas < 0 or fechaActual < -1):
+            messages.info(request, 'La fecha de inicio no puede ser mayor que la fecha final')
+            return render(request, "ventas/creardescuentos.html", context,{})
+        try:
+            newDescuento.full_clean()
+        except:
+            messages.info(request, 'Alguno(s) campo(s) no son validos')
+            return render(request, "ventas/creardescuentos.html", context,{})
+        newDescuento.save()
+        messages.success(request, 'EL descuento ha sido creado exitosamente')
+        return redirect(to='ventas:descuentoCrear')
+
+    return render(request, "ventas/creardescuentos.html", context, {})
+
+def modificarDescuentoSubCategoria(request, idCategoria, idSubCategoria):
+    categorias = Categoria.objects.all()
+    subcategorias = SubCategoria.objects.filter(fkCategoria=idCategoria)
+    productos = Producto.objects.filter(fkSubCategoria=idSubCategoria)
+    categoria = idCategoria
+    subcategoria = idSubCategoria
+    context={
+        'categorias':categorias, 
+        'subcategorias':subcategorias, 
+        'productos':productos,
+        'categoria':categoria,
+        'subcategoria':subcategoria,
+        'producto':0
+        }
+    if request.method == 'POST':
+        data = request.POST
+        fechaInicio = data.get('fecha_inicio')
+        fechaFin = data.get('fecha_fin')
+        descuento = data.get('descuento')
+        subcategoria = SubCategoria.objects.get(pkSubCategoria = idSubCategoria)
+        newDescuento = DescuentoSubCategoria(
+            fkSubCategoria = subcategoria,
+            fechaInicio = fechaInicio,
+            fechaFin = fechaFin,
+            porcentajeDescuento = descuento
+        )
+        auxfechafin = datetime.strptime(fechaFin,'%Y-%m-%d')
+        auxfechainicio = datetime.strptime(fechaFin,'%Y-%m-%d')
+        difFechas = (auxfechafin-auxfechainicio).days
+        fechaActual = (auxfechainicio-datetime.today()).days
+        if(difFechas < 0 or fechaActual < -1):
+            messages.info(request, 'La fecha de inicio no puede ser mayor que la fecha final')
+            return render(request, "ventas/creardescuentos.html", context,{})
+        try:
+            newDescuento.full_clean()
+        except:
+            messages.info(request, 'Alguno(s) campo(s) no son validos')
+            return render(request, "ventas/creardescuentos.html", context,{})
+        newDescuento.save()
+        messages.success(request, 'EL descuento ha sido creado exitosamente')
+        return redirect(to='ventas:descuentoCrear')
+
+    return render(request, "ventas/creardescuentos.html", context, {})
+
+
+def modificarDescuentoProducto(request, idCategoria, idSubCategoria, idProducto):
     categorias = Categoria.objects.all()
     subcategorias = SubCategoria.objects.filter(fkCategoria=idCategoria)
     productos = Producto.objects.filter(fkSubCategoria=idSubCategoria)
